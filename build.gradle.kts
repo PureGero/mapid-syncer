691ff9353491adca2dc5881b429fc5068d253c74
PureGero <puregero@gmail.com>
2026-04-24 14:21:00 +0900

    26.1.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,18 +1,18 @@
 plugins {
     `java-library`
-    id("io.papermc.paperweight.userdev") version "2.0.0-beta.17"
+    id("io.papermc.paperweight.userdev") version "2.0.0-beta.21"
 }
 
 dependencies {
-    paperweight.paperDevBundle(providers.gradleProperty("minecraftVersion"))
+    paperweight.paperDevBundle(providers.gradleProperty("paperVersion"))
 }
 
 java {
-    toolchain.languageVersion.set(JavaLanguageVersion.of(21))
+    toolchain.languageVersion.set(JavaLanguageVersion.of(25))
 }
 
 tasks.withType<JavaCompile> {
-    options.release = 21
+    options.release = 25
 }
 
 tasks.processResources {
